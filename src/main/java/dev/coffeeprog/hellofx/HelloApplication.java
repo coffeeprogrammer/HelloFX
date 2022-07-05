86f71d683f742f3d94514358cce7941aa37071eb
Chad Botting <coffeeprogrammer0@gmail.com>
2022-07-05 18:38:04 -0400

    done lambdaevents

--- src/main/java/dev/coffeeprog/hellofx/HelloApplication.java
+++ src/main/java/dev/coffeeprog/hellofx/HelloApplication.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,15 +16,21 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Button btn = new Button("Click me");
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Button exit = new Button("Exit");
+        exit.setOnAction(e -> {
+            System.out.println("exiting");
+            System.exit(0);
+        } );
+
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 System.out.println("hello world");
             }
         });
-
+        VBox root = new VBox();
+        //root.getChildren().add(btn);
+        root.getChildren().addAll(btn, exit);
         Scene scene = new Scene(root,500,300);
         primaryStage.setScene(scene);
         primaryStage.show();
