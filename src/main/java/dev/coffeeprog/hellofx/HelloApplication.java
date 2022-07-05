ba48e169e0257d9145ac69a208855d9398ab83c2
Chad Botting <coffeeprogrammer0@gmail.com>
2022-07-05 19:22:52 -0400

    Please enter the commit message for your changes. Lines starting

--- src/main/java/dev/coffeeprog/hellofx/HelloApplication.java
+++ src/main/java/dev/coffeeprog/hellofx/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -15,23 +16,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Button btn = new Button("Click me");
-        Button exit = new Button("Exit");
-        exit.setOnAction(e -> {
-            System.out.println("exiting");
-            System.exit(0);
-        } );
-
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                System.out.println("hello world");
-            }
-        });
-        VBox root = new VBox();
-        //root.getChildren().add(btn);
-        root.getChildren().addAll(btn, exit);
-        Scene scene = new Scene(root,500,300);
+        //Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        FXMLLoader root = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(root.load());
+        primaryStage.setTitle("My Title");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
